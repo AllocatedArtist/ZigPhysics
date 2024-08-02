52285160739e7308a1c11f65c2492b2b6cc0335d
Allocated Artist <Senorsiete2006@gmail.com>
2024-08-01 18:10:25 -0700

    add testing

--- build.zig
+++ build.zig
@@ -28,4 +28,20 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const test_compile = b.addTest(.{
+        .name = "physics tests",
+        .root_source_file = b.path("src/test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    test_compile.linkLibrary(raylib_artifact);
+    test_compile.root_module.addImport("raylib", raylib);
+
+    const test_run = b.addRunArtifact(test_compile);
+
+    const test_step = b.step("test", "Run tests");
+
+    test_step.dependOn(&test_run.step);
 }
